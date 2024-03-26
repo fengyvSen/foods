2c7917880167c93a8793981aa7f650ff29b2f6a2
风与 <11443815+the19thfengyv@user.noreply.gitee.com>
2024-03-27 07:15:21 +0800

    🌟：category-menu-right-iitem

--- pages/category/category.vue
+++ pages/category/category.vue
@@ -6,26 +6,30 @@
 		</u-navbar>
 		<view class="category-menu">
 			<view class="menu-left">
-				<view class="menu-left-item" v-for="(item, index) in categoryList" :key="index"
+				<view class="menu-left-item" v-for="(item, index) in categoryList" :key="item.id"
 					:class="{ 'active': index == currentIndex }" @click="currentIndex = index">
 					{{ item.name }}
 				</view>
 			</view>
-			<view class="menu-right">
-				<div class="banner" v-if="isBanner">
-					<u-icon class="banner-close" name="close-circle-fill" color="rgba(0, 0, 0, 0.4)" size="28" @click="closeBanner"></u-icon>
-					<u--image :showLoading="true" :src="bannerSrc" width="80vw" hight="100%" @click="goToBanner"
-						mode="widthFix"></u--image>
-				</div>
-				<view class="menu-right-item">
+			<scroll-view class="menu-right" :scroll-y="true">
+				<view class="banner" v-if="isBanner">
+					<u-icon class="banner-close" name="close-circle-fill" color="rgba(0, 0, 0, 0.4)" size="28"
+						@click="closeBanner"></u-icon>
+					<image :showLoading="true" :src="bannerSrc" @click="goToBanner" mode="widthFix"></image>
+				</view>
+				<view class="menu-right-item" v-for="item in categoryList[currentIndex].cs" v-key="item.id">
 					<view class="tagsname">
-
+						<image class="tagsname-image" :showLoading="true" :src="item.image_url" @click="goToBanner"
+							mode="widthFix"></image>
 					</view>
 					<view class="tagsbox">
-						<view class="tag"></view>
+						<view class=" tagsbox-tag" v-for="(tagitem, index) in item.cs" v-key="tagitem.id"
+							@click="goSearchDetail(tagitem.name)">
+							{{ tagitem.name }}
+						</view>
 					</view>
 				</view>
-			</view>
+			</scroll-view>
 		</view>
 
 	</view>
@@ -33,6 +37,10 @@
 
 <script>
 import { http } from '../../apis/http'
+import { gotoUrl } from '../../utils/utils';
+
+const SEARCHHISTORY = 'searchHistory'
+
 export default {
 	data() {
 		return {
@@ -77,15 +85,40 @@ export default {
 		goToBanner() {
 			this.showToast(
 				{
-						type: 'default',
-						title: '',
-						message: "这是一条广告",
-						iconUrl: 'https://cdn.uviewui.com/uview/demo/toast/default.png'
-					},
+					type: 'default',
+					title: '',
+					message: "这是一条广告",
+					iconUrl: 'https://cdn.uviewui.com/uview/demo/toast/default.png'
+				},
 			)
 		},
-		closeBanner(){
+		closeBanner() {
 			this.isBanner = false
+		},
+		goSearchDetail(kw) {
+			let data = uni.getStorageSync(SEARCHHISTORY) || []
+			// console.log('storage sync', data);
+
+			// let newArr = []				
+			// 考虑去重，本次搜索的是曾经搜素过的，进行去重
+			// for (let i = 0; i < data.length; i++) {
+			// 	   if(data[i] !== kw) {
+			// 		   newArr.push(data[i])
+			// 	   }
+			// }
+			// es6提供的filter方法，过滤方法遍历当前数组生成新的数组
+			data = data.filter(item => item !== kw)	// 返回值为真保留，为假删除
+
+			// 存到localstorage 并不会引起vue的监听
+			uni.setStorage({
+				key: SEARCHHISTORY,
+				data: [kw, ...data]
+			})
+
+
+			uni.navigateTo({
+				url: '/pages/SearchDetail/SearchDetail?kw=' + kw
+			})
 		}
 	},
 	compute: {
@@ -144,20 +177,54 @@ export default {
 	}
 
 	.menu-right {
+		width: 80vw;
+
+		.banner {
+			position: relative;
+			width: 100%;
+
+			.banner-close {
+				z-index: 1;
+				position: absolute;
+				top: 5px;
+				right: 5px;
+			}
+		}
+
 		.menu-right-item {
-			margin: 0 $uni-spacing-row-lg;
+			padding: 0 $uni-spacing-row-lg;
+			margin-bottom: $uni-spacing-col-lg*2;
+
+			.tagsname {
+				width: 100%;
+				margin-bottom: $uni-spacing-col-lg;
+
+				.tagsname-image {
+					width: 100%;
+				}
+			}
+
+			.tagsbox {
+				display: grid;
+				justify-content: center;
+				grid-template-columns: 1fr 1fr 1fr;
+				grid-gap: $uni-spacing-col-sm $uni-spacing-row-sm;
+
+				.tagsbox-tag {
+					box-sizing: border-box;
+					padding: 4px 10px;
+					border: 1rpx solid rgba(7, 17, 27, .1);
+					border-radius: 8px;
+					white-space: nowrap;
+					font-size: 12px;
+					text-align: center;
+				}
+			}
 		}
+
+
 	}
 
-	.banner {
-		position: relative;
 
-		.banner-close {
-			z-index: 1;
-			position: absolute;
-			top: 5px;
-			right: 5px;
-		}
-	}
 }
 </style>
